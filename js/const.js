a2c6a42833766e8344a8ee1149da4b362849e475
barkotenicholas <barkotenicholas@gmail.com>
2022-01-17 03:54:48 +0300

    Added constants

---
+++ js/const.js
@@ -0,0 +1,5 @@
+const pizzasizeprices = [{size:"small",price:200},{size:"medium",price:250},{size:"large",price:300}];
+
+const toppings = [{topping:"Potato",price:50},{topping:"sausage",price:50},{topping:"Capers",price:50},{topping:"Gorgonzola",price:50},{topping:"anchovies",price:50}];
+
+const pizzacrust = [{crust:"Crispy", price:60},{crust:"Stuffed",price:60},{crust:"Gluten-free",price:60}];
